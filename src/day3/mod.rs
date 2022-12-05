e569f6d0a7ff0ab2713c4e048a146704ff4d86a2
diegodox <android.mxdiego9@gmail.com>
2022-12-05 20:45:41 +0900

    better day3

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -1,36 +1,10 @@
-use itertools::{self, Itertools};
 use std::io::BufRead;
-
-fn input() -> std::io::BufReader<std::fs::File> {
-    let mut p = std::path::PathBuf::from(crate::CARGO_MANIFEST_DIR);
-    p.push("src");
-    p.push(module_path!().split("::").last().unwrap());
-    p.push("input.txt");
-    std::io::BufReader::new(std::fs::File::open(p).unwrap())
-}
 
 pub fn part1() -> usize {
     input()
         .lines()
-        .map(|lines| lines.unwrap())
-        .map(|l| {
-            let len = l.len();
-            let (first, last) = l.split_at(len / 2);
-            ('a'..='z')
-                .into_iter()
-                .chain('A'..='Z')
-                // .inspect(|x| println!("{x}"))
-                .filter(|c| first.contains(*c) && last.contains(*c))
-                // .inspect(|c| println!("{c}"))
-                .map(|c| match c {
-                    c @ 'a'..='z' => c as u8 - 'a' as u8 + 1,
-                    c @ 'A'..='Z' => c as u8 - 'A' as u8 + 1 + 26,
-                    _ => unreachable!(),
-                })
-                .map(|x| x as usize)
-                .sum::<usize>()
-        })
-        // .take(1)
+        .map(|line| line.unwrap())
+        .map(part1_line)
         .sum()
 }
 
@@ -38,29 +12,25 @@ pub fn part2() -> usize {
     input()
         .lines()
         .map(|lines| lines.unwrap())
-        .chunks(3)
-        .into_iter()
-        .map(|l| {
-            let l = l.collect_vec();
-            ('a'..='z')
-                .into_iter()
-                .chain('A'..='Z')
-                // .inspect(|x| println!("{x}"))
-                .filter(|c| l.iter().all(|l| l.contains(*c)))
-                // .inspect(|c| println!("{c}"))
-                .map(|c| match c {
-                    c @ 'a'..='z' => c as u8 - 'a' as u8 + 1,
-                    c @ 'A'..='Z' => c as u8 - 'A' as u8 + 1 + 26,
-                    _ => unreachable!(),
-                })
-                .map(|x| x as usize)
-                .sum::<usize>()
-        })
-        // .take(1)
+        .chunk3()
+        .map(|(l1, l2, l3)| part2_chunk(l1, l2, l3))
         .sum()
 }
 
-pub struct Chunk3<I> {
+trait Iterator3ChunkExt {
+    type Iterator;
+    fn chunk3(self) -> Chunk3<Self::Iterator>;
+}
+
+impl<I: Iterator> Iterator3ChunkExt for I {
+    type Iterator = Self;
+
+    fn chunk3(self) -> Chunk3<Self::Iterator> {
+        Chunk3 { iter: self }
+    }
+}
+
+struct Chunk3<I> {
     iter: I,
 }
 
